832072776e637d84bb4b8c6d5c8f8fe395d2fee1
sandipan <sikdarsandipan99@gmail.com>
2017-06-30 09:27:48 +0530

    commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+rm thesis_sandipan.aux
+rm thesis_sandipan.idx
+rm thesis_sandipan.lof
+rm thesis_sandipan.log
+rm thesis_sandipan.lot
+rm thesis_sandipan.out
+rm thesis_sandipan.toc
+pdflatex thesis_sandipan.tex 
+bibtex thesis_sandipan.aux 
+pdflatex thesis_sandipan.tex 
+pdflatex thesis_sandipan.tex
+rm thesis_sandipan.aux
---
+++ texfiles/Chapter_1/run.sh
@@ -0,0 +1,12 @@
+pdflatex template.tex
+bibtex template.aux
+pdflatex template.tex
+pdflatex template.tex
+
+rm *.backup
+rm *.blg
+rm *.log
+rm *.aux
+rm *.bbl
+rm *.end
+
